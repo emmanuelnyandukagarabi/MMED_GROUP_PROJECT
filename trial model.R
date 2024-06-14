75acf0e45f9cf6235319c67217985573212be049
Tshirundu Khangweleni Jameson <156892503+Nkhangwe@users.noreply.github.com>
2024-06-14 21:24:00 +0200

    Add files via upload

--- trial model.R
+++ trial model.R
@@ -1,5 +1,33 @@
 library(deSolve)
 # Function to calculate beta
+load("C:\\Users\\tshir\\Downloads\\dataLondon.Rdata")
+str(London)
+tri<-London
+measles_plot2 <- ggplot(London, aes(x = year, y = meas.inc)) +
+  geom_point(color = "red") +
+  geom_line(color = "red") +
+  labs(title = "Measles in London: Annual incidence before vaccination",
+       x = "Year",
+       y = "Annual incidence (cases per 100,000 population)") +
+  theme_minimal()
+print(measles_plot2)
+measles_plot <- ggplot(London[0:20,], aes(x = year, y = meas.inc)) +
+  geom_point(color = "red") +
+  geom_line(color = "red") +
+  labs(title = "Measles in London: Annual incidence before vaccination",
+       x = "Year",
+       y = "Annual incidence (cases per 100,000 population)") +
+  theme_minimal()
+print(measles_plot)
+measles_plot1 <- ggplot(London[21:46,], aes(x = year, y = meas.inc)) +
+  geom_point(color = "red") +
+  geom_line(color = "red") +
+  labs(title = "Measles in London: Annual incidence after vaccination",
+       x = "Year",
+       y = "Annual incidence (cases per 100,000 population)") +
+  theme_minimal()
+print(measles_plot1)
+
 beta_calc <- function(R_0, mu = 0.02 / 365.25, sigma = 1 / 8, gamma = 1 / 5) {
   R_0 / ((sigma / (mu + sigma)) * (1 / (mu + gamma)))
 }
@@ -25,7 +53,7 @@ R.star <- function(beta, N, mu = 0.02 / 365.25, sigma = 1 / 8, gamma = 1 / 5) {
 }
 
 # Set initial conditions
-initial_conditions <- c(S = 1000, E = 0, I = 1, R = 0)
+initial_conditions <- c(S = 2000, E = 0, I = 1, R = 0)
 
 N0 = initial_conditions[1] + initial_conditions[2] + initial_conditions[3] + initial_conditions[4]
 
@@ -74,10 +102,10 @@ seir <- function(t, y, params) {
 
 
 # Set parameter values
-parameters <- c(beta_calc(13), sigma = 1/8, gamma = 1/5, mu = 0.02, nu = 0.02*N0[[1]], p = 0.2)
+parameters <- c(beta_calc(13), sigma = 1/8, gamma = 1/5, mu = 0.02, nu = 0.02*N0[[1]], p = 1)
 
 # Set time range
-time_range <- seq(0, 100, by = 1)
+time_range <- seq(0, 70, by = 1)
 
 # Solve the ODE system
 solution <- lsoda(initial_conditions, time_range, seir_model, parameters)
@@ -88,8 +116,10 @@ solution_matrix <- solution[, 2:5]
 colnames(solution_matrix) <- c("S", "E", "I", "R")
 
 # Plot the solution
-matplot(time_range, solution_matrix, type = "l", xlab = "Time", ylab = "Population",
-        main = "SEIR Model", lty = 1, lwd = 2, col = c("black", "green", "red", "blue"))
-legend("topright", legend = c("Susceptible", "Exposed", "Infected", "Recovered"),
-       col = c("black", "green", "red", "blue"), lty = 1)
+
+matplot(time_range, solution_matrix, type = "l", xlab = "Time (years)", ylab = "Population",
+        main = paste("SEIR Model : p =",parameters[length(parameters)]), lty = 1, lwd = 2, col = c("black", "green", "red", "blue"))
+legend("top", legend = c("Susceptible", "Exposed", "Infected", "Recovered"),
+       col = c("black", "green", "red", "blue"), lty = 1, bty = "n")
+
  
